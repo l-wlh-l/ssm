872b9985d9da6826a3cc41d4b58abaa7adf0460f
wangluhua <825828826@qq.com>
2019-03-05 15:19:34 +0800

    Projuct的增删改查

--- ssm_parent/ssm_web/src/main/java/com/wlh/ssm/controller/productController/ProductController.java
+++ ssm_parent/ssm_web/src/main/java/com/wlh/ssm/controller/productController/ProductController.java
@@ -2,15 +2,12 @@ package com.wlh.ssm.controller.productController;
 
 import com.wlh.ssm.domain.Product;
 import com.wlh.ssm.service.ProductService;
-import org.apache.ibatis.annotations.Update;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 /**
  * @ClassName ProductController
